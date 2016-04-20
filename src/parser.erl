62444e1327dbfda67f51f5a44ce98b8976a020c8
Koropatnik <koropatnik@yandex.ru>
2016-04-20 18:07:10 +0300

    added parser of tree structure

--- src/parser.erl
+++ src/parser.erl
@@ -1,6 +1,5 @@
 %%%-------------------------------------------------------------------
 %%% @author nikolai
-%%% @copyright (C) 2016, <COMPANY>
 %%% @doc
 %%%
 %%% @end
@@ -11,18 +10,67 @@
 
 %% API
 -export([parseTree/1, start/0]).
+-include_lib("eunit/include/eunit.hrl").
 
+%% @doc Function for parsing list of elements like {element, Value, Level} to tree structure
+%% @spec parseTree(A::List) -> Result
+%%       List = [Item]
+%%       Item = {{element, term(), integer()}}
 parseTree([]) -> [];
 parseTree(Arr) when is_list(Arr) -> parseTree(Arr, 1, []).
 parseTree([], _, Acc) -> Acc;
-parseTree([{element, Value, Level}|T], Level, Acc) -> parseTree(T, Level, Acc ++ [{element, Value, parseTree(T, Level + 1, [])}]);
+parseTree([{element, Value, Level}|T], Level, Acc) ->
+  parseTree(T, Level, Acc ++ [{element, Value, parseTree(T, Level + 1, [])}]);
 parseTree([{element, _Value, Level}|T], Level2, Acc) when Level > Level2 -> parseTree(T, Level2, Acc);
 parseTree([{element, _Value, _Level}|_T], _, Acc) -> Acc.
 
 start() ->
   A = parseTree([{element, 1, 1}, {element, 2, 1},
-                    {element, 3, 2}, {element, 4, 2}, {element, 11, 3}, {element, 5, 2},
-                {element, 6, 1}, {element, 7, 1}, {element, 8, 2}, {element, 9, 3}, {element, 10, 1}]),
+    {element, 3, 2}, {element, 4, 2}, {element, 11, 3}, {element, 5, 2},
+    {element, 6, 1}, {element, 7, 1}, {element, 8, 2}, {element, 9, 3}, {element, 10, 1}]),
   A.
 
-%%([{element, 1, 1}, {element, 2, 1}, {element, 3, 2}, {element, 4, 2}, {element, 5, 2}, {element, 6, 1}])
+%% Tests
+parseTree_normal1_test() ->
+  [{element, 1, []},
+    {element, 2, [
+      {element, 3, []},
+      {element, 4, []},
+      {element, 5, []}]},
+    {element, 6, []}] =
+    parseTree([
+      {element, 1, 1},
+      {element, 2, 1},
+        {element, 3, 2},
+        {element, 4, 2},
+        {element, 5, 2},
+      {element, 6, 1}]).
+
+parseTree_normal2_test() ->
+  [
+    {element, 1, []},
+    {element, 2, [
+      {element, 3, []},
+      {element, 4, [
+        {element, 11, []}
+      ]},
+      {element, 5, []}]},
+    {element, 6, []},
+    {element, 7, [
+      {element, 8, [
+        {element, 9, []}]}]},
+    {element, 10, []}] =
+    parseTree([
+      {element, 1, 1},
+      {element, 2, 1},
+      {element, 3, 2},
+      {element, 4, 2},
+      {element, 11, 3},
+      {element, 5, 2},
+      {element, 6, 1},
+      {element, 7, 1},
+      {element, 8, 2},
+      {element, 9, 3},
+      {element, 10, 1}]).
+
+parseTree_empty_test() -> [] = parseTree([]).
